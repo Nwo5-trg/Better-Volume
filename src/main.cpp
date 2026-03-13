ff3299b5ff9e2be86c5b14a262a6f0313de5d78f
lyyako <lyyako@proton.me>
2026-03-13 23:59:36 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,191 +1,95 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/modify/OptionsLayer.hpp>
-#include <Geode/modify/SliderTouchLogic.hpp>
 
 using namespace geode::prelude;
 
-std::string ftofstr(float num, int decimal) {
-    if (decimal == 0) return std::to_string((int)round(num));
-    std::ostringstream ss;
-    ss << std::fixed << std::setprecision(decimal) << num;
-    std::string string = ss.str();
-    string.erase(string.find_last_not_of('0') + 1, std::string::npos);
-    if (string.back() == '.') string.pop_back();
-    return string;
+std::string getStr(float val) {
+    return std::to_string(static_cast<int>(std::round(val * 100.f)));
 }
 
-class $modify(MySliderTouchLogic, SliderTouchLogic) {
-    struct Fields {
-        std::function<void(float)> m_callback;
+template <typename T>
+void setupVolumeUI(T* layer, Slider*& mSl, Slider*& sSl, TextInput*& mIn, TextInput*& sIn) {
+    auto mod = Mod::get();
+    bool doColor = mod->getSettingValue<bool>("colored-bars");
+    auto mCol = mod->getSettingValue<ccColor3B>("music-color");
+    auto sCol = mod->getSettingValue<ccColor3B>("sfx-color");
+    auto tex = CCTextureCache::sharedTextureCache()->addImage("sliderBar2.png", true);
+
+    mSl = typeinfo_cast<Slider*>(layer->getChildByIDRecursive("music-slider"));
+    sSl = typeinfo_cast<Slider*>(layer->getChildByIDRecursive("sfx-slider"));
+    auto mLbl = typeinfo_cast<CCLabelBMFont*>(layer->getChildByIDRecursive("music-label"));
+    auto sLbl = typeinfo_cast<CCLabelBMFont*>(layer->getChildByIDRecursive("sfx-label"));
+
+    if (!mSl || !sSl || !mLbl || !sLbl) return;
+
+    auto setupInput = [&](Slider* sl, CCLabelBMFont* lbl, TextInput*& in, ccColor3B col, bool isM) {
+        if (doColor && tex) { sl->m_sliderBar->setTexture(tex); sl->m_sliderBar->setColor(col); }
+
+        lbl->setPositionX(lbl->getPositionX() - 22.f);
+        sl->setZOrder(1);
+
+        in = TextInput::create(40.f, "0");
+        in->setPosition(lbl->getPositionX() + lbl->getScaledContentSize().width / 2.f + 20.f, lbl->getPositionY());
+        in->setScale(0.65f);
+        in->setFilter("1234567890");
+        in->setString(getStr(sl->getValue()));
+
+        in->setCallback([layer, sl, isM](const std::string& val) {
+            if (val.empty()) return;
+            sl->setValue(std::clamp(std::strtof(val.c_str(), nullptr), 0.f, 100.f) / 100.f);
+            sl->updateBar();
+            isM ? layer->musicSliderChanged(sl->m_touchLogic->m_thumb) : layer->sfxSliderChanged(sl->m_touchLogic->m_thumb);
+        });
+
+        if (auto p = lbl->getParent()) {
+            p->addChild(in);
+
+            auto pct = CCLabelBMFont::create("%", lbl->getFntFile());
+            pct->setPosition(in->getPositionX() + 28.f, in->getPositionY());
+            pct->setScale(lbl->getScale());
+            p->addChild(pct);
+        }
     };
 
-    void setCallback(std::function<void(float)> callback) {
-        m_fields->m_callback = callback;
-    }
+    setupInput(mSl, mLbl, mIn, mCol, true);
+    setupInput(sSl, sLbl, sIn, sCol, false);
+}
 
-    void ccTouchMoved(cocos2d::CCTouch* p0, cocos2d::CCEvent* p1) {
-        SliderTouchLogic::ccTouchMoved(p0, p1);
-        if (m_fields->m_callback) m_fields->m_callback(m_thumb->getValue());
-    }
-};
-
-class $modify(PauseMenu, PauseLayer) {
-    struct Fields {
-        TextInput* musicInput;
-        TextInput* sfxInput;
-        Slider* musicSlider;
-        Slider* sfxSlider;
-        int64_t rounding;
-    };
+class $modify(MyPauseLayer, PauseLayer) {
+    struct Fields { TextInput* mIn; TextInput* sIn; Slider* mSl; Slider* sSl; };
 
     void customSetup() {
         PauseLayer::customSetup();
-
-        bool colorBars = Mod::get()->getSettingValue<bool>("colored-bars");
-        ccColor3B musicColor = Mod::get()->getSettingValue<ccColor3B>("music-color");
-        ccColor3B sfxColor = Mod::get()->getSettingValue<ccColor3B>("sfx-color");
-        m_fields->rounding = Mod::get()->getSettingValue<int64_t>("rounding");
-        std::string musicCustomText = Mod::get()->getSettingValue<std::string>("music-text");
-        std::string sfxCustomText = Mod::get()->getSettingValue<std::string>("sfx-text");
-
-        auto musicSlider = typeinfo_cast<Slider*>(this->getChildByIDRecursive("music-slider"));
-        auto sfxSlider = typeinfo_cast<Slider*>(this->getChildByIDRecursive("sfx-slider"));
-        auto musicText = typeinfo_cast<CCLabelBMFont*>(this->getChildByIDRecursive("music-label"));
-        auto sfxText = typeinfo_cast<CCLabelBMFont*>(this->getChildByIDRecursive("sfx-label"));
-
-        if (!musicSlider || !sfxSlider || !musicText || !sfxText) return;
-
-        m_fields->musicSlider = musicSlider;
-        m_fields->sfxSlider = sfxSlider;
-
-        CCTexture2D* colorableTexture = CCTextureCache::sharedTextureCache()->addImage("sliderBar2.png", true);
-        musicText->setString((musicCustomText + "      %").c_str());
-        sfxText->setString((sfxCustomText + "      %").c_str());
-
-        for (auto slider : {musicSlider, sfxSlider}) {
-            bool isMusic = (slider == musicSlider);
-
-            if (colorBars) {
-                slider->m_sliderBar->setTexture(colorableTexture);
-                slider->m_sliderBar->setColor(isMusic ? musicColor : sfxColor);
-            }
-
-            auto input = TextInput::create(50.0f, "0");
-            input->setZOrder(1);
-            slider->setZOrder(3);
-            input->setPosition(isMusic ? ccp(musicText->getPositionX() + 16, musicText->getPositionY()) : ccp(sfxText->getPositionX() + 9, sfxText->getPositionY()));
-            input->setScale(0.65f);
-            input->setFilter("1234567890.");
-            
-            input->setCallback([slider, isMusic](const std::string& val) {
-                if (!val.empty()) {
-                    auto value = std::strtof(val.c_str(), nullptr);
-                    if (value <= 100) {
-                        slider->setValue(value / 100);
-                        slider->updateBar();
-                        if (isMusic) FMODAudioEngine::get()->setBackgroundMusicVolume(value / 100);
-                        else FMODAudioEngine::get()->setEffectsVolume(value / 100);
-                    }
-                }
-            });
-            
-            (isMusic ? m_fields->musicInput : m_fields->sfxInput) = input;
-            this->addChild(input);
-        }
-        
-        updateInputWithSlider(m_fields->musicInput, musicSlider);
-        updateInputWithSlider(m_fields->sfxInput, sfxSlider);
+        setupVolumeUI(this, m_fields->mSl, m_fields->sSl, m_fields->mIn, m_fields->sIn);
     }
 
     void musicSliderChanged(CCObject* p0) {
         PauseLayer::musicSliderChanged(p0);
-        updateInputWithSlider(m_fields->musicInput, m_fields->musicSlider);
+        if (m_fields->mIn && m_fields->mSl) m_fields->mIn->setString(getStr(m_fields->mSl->getValue()));
     }
 
     void sfxSliderChanged(CCObject* p0) {
         PauseLayer::sfxSliderChanged(p0);
-        updateInputWithSlider(m_fields->sfxInput, m_fields->sfxSlider);
-    }
-
-    void updateInputWithSlider(TextInput* input, Slider* slider) {
-        if (!input || !slider) return;
-        input->setString(ftofstr(slider->getValue() * 100, m_fields->rounding));
+        if (m_fields->sIn && m_fields->sSl) m_fields->sIn->setString(getStr(m_fields->sSl->getValue()));
     }
 };
 
 class $modify(MyOptionsLayer, OptionsLayer) {
-    struct Fields {
-        TextInput* musicInput;
-        TextInput* sfxInput;
-        Slider* musicSlider;
-        Slider* sfxSlider;
-    };
+    struct Fields { TextInput* mIn; TextInput* sIn; Slider* mSl; Slider* sSl; };
 
     void customSetup() {
         OptionsLayer::customSetup();
+        setupVolumeUI(this, m_fields->mSl, m_fields->sSl, m_fields->mIn, m_fields->sIn);
+    }
 
-        bool colorBars = Mod::get()->getSettingValue<bool>("colored-bars");
-        ccColor3B musicColor = Mod::get()->getSettingValue<ccColor3B>("music-color");
-        ccColor3B sfxColor = Mod::get()->getSettingValue<ccColor3B>("sfx-color");
-        int64_t rounding = Mod::get()->getSettingValue<int64_t>("rounding");
-        std::string musicCustomText = Mod::get()->getSettingValue<std::string>("music-text");
-        std::string sfxCustomText = Mod::get()->getSettingValue<std::string>("sfx-text");
+    void musicSliderChanged(CCObject* p0) {
+        OptionsLayer::musicSliderChanged(p0);
+        if (m_fields->mIn && m_fields->mSl) m_fields->mIn->setString(getStr(m_fields->mSl->getValue()));
+    }
 
-        auto musicSlider = typeinfo_cast<Slider*>(this->getChildByIDRecursive("music-slider"));
-        auto sfxSlider = typeinfo_cast<Slider*>(this->getChildByIDRecursive("sfx-slider"));
-        auto musicLabel = typeinfo_cast<CCLabelBMFont*>(this->getChildByIDRecursive("music-label"));
-        auto sfxLabel = typeinfo_cast<CCLabelBMFont*>(this->getChildByIDRecursive("sfx-label"));
-
-        if (!musicSlider || !sfxSlider || !musicLabel || !sfxLabel) return;
-
-        m_fields->musicSlider = musicSlider;
-        m_fields->sfxSlider = sfxSlider;
-
-        musicLabel->setString((musicCustomText + "      %").c_str());
-        sfxLabel->setString((sfxCustomText + "      %").c_str());
-
-        CCTexture2D* colorableTexture = CCTextureCache::sharedTextureCache()->addImage("sliderBar2.png", true);
-
-        for (auto slider : {musicSlider, sfxSlider}) {
-            bool isMusic = (slider == musicSlider);
-
-            if (colorBars) {
-                slider->m_sliderBar->setTexture(colorableTexture);
-                slider->m_sliderBar->setColor(isMusic ? musicColor : sfxColor);
-            }
-
-            auto input = TextInput::create(50.0f, "0");
-            input->setZOrder(1);
-            slider->setZOrder(3);
-            input->setPosition(isMusic ? ccp(musicLabel->getPositionX() + 16, musicLabel->getPositionY()) : ccp(sfxLabel->getPositionX() + 9, sfxLabel->getPositionY()));
-            input->setScale(0.65f);
-            input->setFilter("1234567890."); 
-            input->setString(ftofstr(slider->getValue() * 100, rounding));
-
-            input->setCallback([this, slider, isMusic](const std::string& val) {
-                if (!val.empty()) {
-                    auto value = std::strtof(val.c_str(), nullptr);
-                    if (value <= 100) {
-                        slider->setValue(value / 100);
-                        slider->updateBar();
-                        if (isMusic) {
-                            FMODAudioEngine::get()->setBackgroundMusicVolume(value / 100);
-                            this->musicSliderChanged(slider->m_touchLogic->m_thumb);
-                        } else {
-                            FMODAudioEngine::get()->setEffectsVolume(value / 100);
-                            this->sfxSliderChanged(slider->m_touchLogic->m_thumb);
-                        }
-                    }
-                }
-            });
-
-            static_cast<MySliderTouchLogic*>(slider->m_touchLogic)->setCallback([input, rounding](float val) {
-                input->setString(ftofstr(val * 100, rounding));
-            });
-
-            slider->getParent()->addChild(input);
-            (isMusic ? m_fields->musicInput : m_fields->sfxInput) = input;
-        }
+    void sfxSliderChanged(CCObject* p0) {
+        OptionsLayer::sfxSliderChanged(p0);
+        if (m_fields->sIn && m_fields->sSl) m_fields->sIn->setString(getStr(m_fields->sSl->getValue()));
     }
 };
